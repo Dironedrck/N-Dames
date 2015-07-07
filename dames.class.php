ad79f4190cdad78f8a8f3c3585eb2483695a53fd
mahmoud-nb <mahmoud.nb@gmail.com>
2015-07-07 11:49:05 +0100

    graphical layout of the solution
    Add a graphic interface to display the chessboard with a graphical
    layout of the solution

--- dames.class.php
+++ dames.class.php
@@ -1,54 +1,82 @@
 <?php
 
+/**
+ * N.Dames
+ */
+
 class NDames {
 
 	public $n;
 	public $firstPosition;
 	public $dames;
+	public $debug;
 
-	public function __construct($n = 8, $firstPosition=array('x'=>0, 'y'=>0)){
+	public function __construct($n = 8, $firstPosition=array('x'=>0, 'y'=>4), $debug=true){
 
 		$this->n = $n ;
 		$this->firstPosition = $firstPosition;
+		$this->debug = $debug;
 		
 		return $this;
 	}
+	
+	/**
+	 * >>>>>>>>>>>>>># SOLUTION 1
+	 * Run algorithm 
+	 */
 
 	public function run(){
 		$this->dames[0] = $this->firstPosition;
+		if($this->debug) echo '<br />>> PUSH [0] => '. $this->firstPosition['x'].','.$this->firstPosition['y'] ;
 		$this->push(1, $this->firstPosition['x'], $this->firstPosition['y']);
 		return $this->dames;
 	}
 
 	public function push($index, $x, $y){
+		static $currentX;
+		static $currentY;
 		if($index < $this->n){
 			if($newPos = $this->nextValidPosition($x, $y)){
 				$this->dames[$index] = $newPos;
-				echo '<br />##'. $index .' => '. $newPos['x'].','.$newPos['y'] ;
+				if($this->debug) echo '<br />>> PUSH ['. $index .'] => '. $newPos['x'].','.$newPos['y'] ;
 				$this->push($index+1, $newPos['x']+1, 0);
 			}else{
-				echo '<br />OOOPS ['. $index .'] => ??'; 
+				if($this->debug) echo '<br />>> OOPS ['. $index .'] => ?,?'; 
 				
 				$index--;
-				$currentX = $this->dames[$index]['x']; 
-				$currentY = $this->dames[$index]['y'];
-				//echo '<br />['. $index .'] Current X/Y => '. $currentX .' / '. $currentY;
-				
-				array_pop($this->dames);
-				$this->push($index, $currentX, $currentY+1);
+				if($index >= 0){
+					$currentX = $this->dames[$index]['x']; 
+					$currentY = $this->dames[$index]['y'];
+					//if($this->debug) echo '<br />['. $index .'] Current X/Y => '. $currentX .' / '. $currentY;
+					
+					array_pop($this->dames);
+					$this->push($index, $currentX, $currentY+1);
+				}else{
+					$this->dames[$index+1] = array('x'=>$currentX, 'y'=>$currentY);
+				}
 			}
+		}else{
+			echo '<br />>> GOOD :)<br /><br />';
 		}
 	}
 	
+	// *************************************************************************** //
+	
+	/**
+	 * >>>>>>>>>>>>>># SOLUTION 2
+	 * Run algorithm 
+	 */
+	
 	public function run2(){
 		$this->push2(0, $this->firstPosition['x'], $this->firstPosition['y']);
-		echo'<pre>'; print_r($this->dames); echo'</pre>';
 		return $this->dames;
 	}
 	
+	/**
+	 * Push position
+	 */
 	public function push2($index, $x, $y, $push = true){
 		if($index < $this->n){
-			
 			
 			$yy = $y+1;
 			if($push){
@@ -56,20 +84,19 @@ class NDames {
 				$this->dames[$index] = array('x'=>$x, 'y'=>$y);
 			}
 			
-			echo '<br />## '. $index .' >> '. $x.','.$y; // ' NEXT '. ($x+1) .','. $yy;
+			if($this->debug) echo '<br />>> PUSH ['. $index .'] => '. $x.','.$y; // ' NEXT '. ($x+1) .','. $yy;
 			
 			if($newPos = $this->nextValidPosition($x+1, $yy)){
-				//echo'<pre>'; print_r($newPos); echo'</pre>';
+				//if($this->debug) echo'<pre>'; print_r($newPos); echo'</pre>';
 				$index++;
 				$this->push2($index, $newPos['x'], $newPos['y']);
 			}else{
-				echo '<br />OOOPS ['. ($index+1) .'] => ??'; 
+				if($this->debug) echo '<br />>> OOPS ['. ($index+1) .'] => ?,?'; 
 				
 				$lastDame = end($this->dames);
 				
 				$currentX = $lastDame['x']; 
 				$currentY = $lastDame['y'];
-				echo '<br />['. $index .'] Current X/Y => '. $currentX .' / '. $currentY;
 				
 				//$index--;
 				array_pop($this->dames);
@@ -77,10 +104,14 @@ class NDames {
 			}
 		}
 	}
+	
+	// *************************************************************************** //
 
+	/**
+	 * Get first next valid position from $minX, $minY
+	 */
 	public function nextValidPosition($minX, $minY){
 
-		//echo '<br />nextValidPosition >> '. "$minX, $minY<br />";
 		for($x=$minX ; $x < $this->n ; $x++){
 			for($y=$minY ; $y < $this->n ; $y++){
 
@@ -90,9 +121,11 @@ class NDames {
 
 			}
 		}
-
 	}
-
+	
+	/**
+	 * Check position
+	 */
 	public function isValid($x, $y){
 
 		foreach ($this->dames as $key => $dame) {
@@ -103,10 +136,4 @@ class NDames {
 		return true ;
 	}
 }
-
-
-
-
-
-
 
--- index.php
+++ index.php
@@ -11,21 +11,25 @@
 	
 	<body>
 	<?php
-		$n = 5;
-	
-		$d = new NDames($n);
+		$n = isset($_GET['n']) ? $_GET['n'] : 3;
+		$startX = 0;
+		$startY = isset($_GET['y']) ? $_GET['y'] : 0;
+		$start = array('x'=>$startX, 'y'=>$startY);
+		$d = new NDames($n, $start);
 		$dames = $d->run();
-		echo'<pre>'; print_r($dames); echo'</pre>';
+		//echo'<pre>'; print_r($dames); echo'</pre>';
 	?>
 	
-	<?php if($dames){ ?>
+	<?php if($dames){ $d = 0; ?>
 
 		<table id="chess_board" cellpadding="0" cellspacing="0">
 			<?php for($i=0 ; $i<$n ; $i++){ ?>
 			<tr>
 				<?php for($j=0 ; $j<$n ; $j++){ ?>
 					<td>
-						<?php /* <a href="#" class="king black">&#9819;</a> */ ?>
+						<?php if(isset($dames[$d]) && $dames[$d]['x'] == $i && $dames[$d]['y'] == $j){ ?>
+							<a href="#" class="king black">&#9819;</a>
+						<?php $d++; } ?>
 					</td>
 				<?php } ?>
 			</tr>
